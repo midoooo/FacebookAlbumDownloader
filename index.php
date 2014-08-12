45d3292eef827b8bec82269e6b6221e886e0a3e1
mouyse <johnson.shah@gmail.com>
2014-08-12 13:08:26 +0530

    CSS/styles.css

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
 <title>Facebook Album Downloader</title>
 <meta charset="utf-8">
+<meta name="description" content="Login to your facebook account to download any of your albums">
+<meta name="author" content="Mouyse">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -13,12 +15,42 @@
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="bootstrap/js/bootstrap.min.js"></script>
 
+<!-- Custom CSS -->
+<script src="css/styles.css"></script>
+
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 </head>
 
 <body>
-<h1>Hello, world!</h1>
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <div class="container">
+	<div class="navbar-header">
+	  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+	  <a class="navbar-brand" href="#">Facebook Album Downloader</a>
+	</div>
+	<!--<div class="collapse navbar-collapse">
+	  <ul class="nav navbar-nav">
+		<li class="active"><a href="#">Home</a></li>
+		<li><a href="#about">About</a></li>
+		<li><a href="#contact">Contact</a></li>
+	  </ul>
+	</div><!--/.nav-collapse -->-->
+  </div>
+</div>
+ <div class="container">
+
+      <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
+
+    </div>
 </body>
 
 </html> 
