fd5ca88916bb1fbb9ca74f196cf87341de4fc16d
mouyse <johnson.shah@gmail.com>
2014-08-12 09:43:47 +0530

    Bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 FacebookAlbumDownloader
 =======================
+
+Libraries used with their links:
+---------------------------------------------
+Bootstrap (3.2.0) Link: http://getbootstrap.com/ 
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Facebook Album Downloader</title>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+</body>
+
+</html> 
