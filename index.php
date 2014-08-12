71b16d8fb50e23a6b9eb172007d3f47e4ec811cd
mouyse <johnson.shah@gmail.com>
2014-08-12 12:55:41 +0530

    Hello World

--- index.php
+++ index.php
@@ -2,20 +2,23 @@
 <html>
 <head>
 <title>Facebook Album Downloader</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-
 <!-- Optional theme -->
 <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
-
-<!-- Latest compiled and minified JavaScript -->
+<!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="bootstrap/js/bootstrap.min.js"></script>
 
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 </head>
 
 <body>
-
+<h1>Hello, world!</h1>
 </body>
 
 </html> 
